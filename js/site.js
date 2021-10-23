bdb8b90fc26b9b2cf2e7b6bf14466efae864f170
piaomu <driftwood.wahl@gmail.com>
2021-10-23 16:35:46 -0500

    first commit

---
+++ js/site.js
@@ -0,0 +1,22 @@
+// These functions perform simple arithmetic.
+function addDigits()
+{
+
+}
+
+function substractDigits()
+{
+
+}
+
+function multiplyDigits()
+{
+
+}
+
+function divideDigits()
+{
+
+}
+
+// These functions translate the result into Chinese
