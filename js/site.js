7634f5d4a974c003af270ebd36f362f0592b221e
piaomu <driftwood.wahl@gmail.com>
2021-10-24 15:51:36 -0500

    Business Logic

--- js/site.js
+++ js/site.js
@@ -1,22 +1,36 @@
-// These functions perform simple arithmetic.
-function addDigits()
+let numOne = document.getElementById("numOne").value;
+let numTwo = document.getElementById("numTwo").value;
+
+document.getElementById("submitBtn").addEventListener("click", ()=>
 {
 
+let add = document.getElementById("Add");
+let subtract = document.getElementById("Subtract");
+let multiply = document.getElementById("Multiply");
+let divide = document.getElementById("Divide");
+})
+
+
+// Simple arithmetic.
+function addDigits()
+{
+  return numOne + numTwo;
 }
 
 function substractDigits()
 {
-
+  return numOne - numTwo;
 }
 
 function multiplyDigits()
 {
-
+  return numOne * numTwo;
 }
 
+// Don't forget to round this number.
 function divideDigits()
 {
-
+  return numOne / numTwo;
 }
 
 // These functions translate the result into Chinese
