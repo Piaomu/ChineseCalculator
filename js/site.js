229b032bec256bce7d66bfb6f162ca5eb2d4276b
piaomu <driftwood.wahl@gmail.com>
2021-10-25 13:27:30 -0500

    valueMap

--- js/site.js
+++ js/site.js
@@ -3,13 +3,66 @@ let numTwo = document.getElementById("numTwo").value;
 
 document.getElementById("submitBtn").addEventListener("click", ()=>
 {
+doMath();
 
+})
+
+function doMath()
+{
 let add = document.getElementById("Add");
 let subtract = document.getElementById("Subtract");
 let multiply = document.getElementById("Multiply");
 let divide = document.getElementById("Divide");
-})
+let result = "";
 
+if (add.checked)
+{
+  result = addDigits();
+} else if (subtract.checked)
+{
+  result = subtractDigits();
+} else if (multiply.checked)
+{
+  result = multiplyDigits();
+} else if (divide.checked)
+{
+  result = divideDigits();
+};
+
+return parseInt(result);
+}
+
+function translateToChinese()
+{
+// Tranlation Map
+const valueMap = new Map();
+
+valueMap.set(0, "零");
+valueMap.set(1, "一");
+valueMap.set(2, "二");
+valueMap.set(3, "三");
+valueMap.set(4, "四");
+valueMap.set(5, "五");
+valueMap.set(6, "六");
+valueMap.set(7, "七");
+valueMap.set(8, "八");
+valueMap.set(9, "九");
+valueMap.set(10, "十");
+valueMap.set(100, "百");
+valueMap.set(1000, "千");
+
+// Output Variables
+let result = doMath();
+let output = "";
+
+if(result === 0 && result < 10)
+{
+output = valueMap.get(result);
+} else if(result > 10 && result < 20)
+{
+  output = (valueMap.get(10)) + (valueMap.get(result-10));
+} 
+}
 
 // Simple arithmetic.
 function addDigits()
